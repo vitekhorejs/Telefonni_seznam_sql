023e85d77f80a4f92051e7cd505bcd5013c9fe38
horejvi14 <horejvi14@DST20716>
2017-03-07 11:22:14 +0100

    sqlite-net-pcl

---
+++ Telefonní seznam/Telefonní_seznam/FileHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.IO;
+
+using Xamarin.Forms;
+
+namespace Telefonní_seznam
+{
+    class FileHelper
+    {
+        public string GetLocalFilePath(string filename)
+        {
+            return Path.Combine("", filename);
+        }
+    }
+}
---
+++ Telefonní seznam/Telefonní_seznam/TodoItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace Telefonní_seznam
+{
+    public class TodoItem
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string RodneCislo { get; set; }
+        public string Gender { get; set; }
+        public string Added { get; set; }
+        public string Edited { get; set; }
+        public int Age
+        {
+            get { return _age(); }
+        }
+        public int _age()
+        {
+            DateTime now = DateTime.Today;
+            int age = now.Year - BirthDate.Year;
+            if (now < BirthDate.AddYears(age)) age--;
+            return age;
+        }
+
+        public override string ToString()
+        {
+            return Name + " " + SurName + "  " + RodneCislo + " " + Age;
+        }
+    }
+}
